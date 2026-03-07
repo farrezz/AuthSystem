035223b77002516a97fdcd9684e351c4d080b877
Farre <farrezzz@gmail.com>
2026-03-07 20:19:10 +0100

    Added user model

---
+++ AuthSystem.API/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthSystem.API.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(200)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+
+    }
+}
