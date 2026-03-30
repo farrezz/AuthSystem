0a3b5879fcd62141ce09d7048c672e197f6c6c0e
Farre <farrezzz@gmail.com>
2026-03-30 20:27:29 +0200

    created DB, services, controller

---
+++ AuthSystem.API/Services/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthSystem.API.Services
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string passwordHash);
+    }
+}
---
+++ AuthSystem.API/Services/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿namespace AuthSystem.API.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            // Hash the password using BCrypt
+            // The HashPassword method automatically generates
+            // a salt and combines it with the password before hashing
+    
+            return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 12);
+        }
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            // Verify the password against the stored hash
+            // The Verify method extracts the salt from the hash and uses it to hash the input password for comparison
+            // It returns true if the password matches the hash, and false otherwise
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+
+    }
+}
